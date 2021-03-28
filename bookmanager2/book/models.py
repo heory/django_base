fce8d9e4fda82e77ea88f08dbcf65829c275e86d
hhy <hehaoyuxyz@126.com>
2021-03-28 23:32:26 +0800

    添加初测数据

--- bookmanager2/book/models.py
+++ bookmanager2/book/models.py
@@ -12,3 +12,18 @@ class BookInfo(models.Model):
         db_table='bookinfo'
         verbose_name='书籍管理'
 
+class PeopleInfo(models.Model):
+    GENDER_CHOICE=(
+        (1, 'male'),
+        (2, 'female')
+    )
+
+    name=models.CharField(max_length=10,unique=True)
+    gender=models.SmallIntegerField(choices=GENDER_CHOICE,default=1)
+    description=models.CharField(max_length=100,null=True)
+    is_delete=models.BooleanField(default=False)
+
+    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table='peopleinfo'
