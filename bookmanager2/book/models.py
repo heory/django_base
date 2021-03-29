70b3ddd86ae96218fea48f4232116e106b6053ef
hhy <hehaoyuxyz@126.com>
2021-03-29 18:00:24 +0800

    数据库查询

--- bookmanager2/book/models.py
+++ bookmanager2/book/models.py
@@ -1,29 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
 class BookInfo(models.Model):
-    name =models.CharField(max_length=10,unique=True)
-    pub_date=models.DateField(null=True)
-    readcount=models.IntegerField(default=0)
-    commentcount=models.IntegerField(default=0)
-    is_delete=models.BooleanField(default=False)
+    name = models.CharField(max_length=10, unique=True)
+    pub_date = models.DateField(null=True)
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
 
     class Meta:
-        db_table='bookinfo'
-        verbose_name='书籍管理'
+        db_table = 'bookinfo'
+        verbose_name = '书籍管理'
+
+    def __str__(self):
+        return self.name
 
 class PeopleInfo(models.Model):
-    GENDER_CHOICE=(
+    GENDER_CHOICE = (
         (1, 'male'),
         (2, 'female')
     )
 
-    name=models.CharField(max_length=10,unique=True)
-    gender=models.SmallIntegerField(choices=GENDER_CHOICE,default=1)
-    description=models.CharField(max_length=100,null=True)
-    is_delete=models.BooleanField(default=False)
+    name = models.CharField(max_length=10, unique=True)
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1)
+    description = models.CharField(max_length=100, null=True)
+    is_delete = models.BooleanField(default=False)
 
-    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
 
     class Meta:
-        db_table='peopleinfo'
+        db_table = 'peopleinfo'
+
+    def __str__(self):
+        return self.name
--- bookmanager2/book/views.py
+++ bookmanager2/book/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from book.models import BookInfo
 def index(request):
+
     return HttpResponse('index')
+
+
