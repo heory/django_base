9e332950cc7d0201cbb4162f55495e5b3cf986a8
hhy <hehaoyuxyz@126.com>
2021-04-01 17:39:12 +0800

    更改mysql密码

--- bookmanager3/bookmanager3/settings.py
+++ bookmanager3/bookmanager3/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST':'localhost',
         'PORT':3306,
         'USER':'root',
-        'PASSWORD':'123456',
+        'PASSWORD':'1295460898',
         'NAME':'book_42_02',
     }
 }
